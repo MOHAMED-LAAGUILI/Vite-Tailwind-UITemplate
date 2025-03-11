7324fa2d919a8e9f8911e10cf59888678d697566
MOHAMED LAAGUILI <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2025-03-11 16:22:01 +0000

    ....

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['ldrs/linespinner']
-    }
-  },
-
   plugins: [react(),
 
     compression({
