2b08533105acd3c1e9a523ba01ec612275112c4a
MOHAMED LAAGUILI <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2025-03-16 15:35:30 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,66 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TextPage from "./Pages/TextPage";
-import SpinnerPage from "./Pages/SpinnerPage";
-import Layout from "./Layout/Layout";
-import DashBoardItems from "./Pages/DashBoardItemsPage";
-import DropDownPage from "./Pages/DropDownPage";
-import AuthPage from "./Pages/AuthPage";
-import TablesPage from "./Pages/TablesPage";
-import BgPage from "./Pages/BgPage";
-import ButtonsPage from "./Pages/ButtonsPage";
-import CalendarPage from './Pages/CalendarPage';
-import ToolBarPage from "./Pages/ToolBarPage";
-import AlertPage from "./Pages/AlertPage";
-import TailwindSourcesPage from "./Pages/TailwindSourcesPage";
-import BlankPage from "./Pages/BlankPage";
-import ProfilePage from "./Pages/ProfilePage";
-import NotFound404 from "./Pages/NotFound404";
-import HeroSectionPage from "./Pages/HeroSectionPage";
-import HomePage from "./Pages/HomePage";
-// import HtmlEditor from "./Pages/HtmlEditor";
+import { Suspense, lazy } from "react";
+import { squircle } from 'ldrs';
+
+// Lazy load the pages
+const TextPage = lazy(() => import("./Pages/TextPage"));
+const SpinnerPage = lazy(() => import("./Pages/SpinnerPage"));
+const Layout = lazy(() => import("./Layout/Layout"));
+const DashBoardItems = lazy(() => import("./Pages/DashBoardItemsPage"));
+const DropDownPage = lazy(() => import("./Pages/DropDownPage"));
+const AuthPage = lazy(() => import("./Pages/AuthPage"));
+const TablesPage = lazy(() => import("./Pages/TablesPage"));
+const BgPage = lazy(() => import("./Pages/BgPage"));
+const ButtonsPage = lazy(() => import("./Pages/ButtonsPage"));
+const CalendarPage = lazy(() => import("./Pages/CalendarPage"));
+const ToolBarPage = lazy(() => import("./Pages/ToolBarPage"));
+const AlertPage = lazy(() => import("./Pages/AlertPage"));
+const TailwindSourcesPage = lazy(() => import("./Pages/TailwindSourcesPage"));
+const BlankPage = lazy(() => import("./Pages/BlankPage"));
+const ProfilePage = lazy(() => import("./Pages/ProfilePage"));
+const NotFound404 = lazy(() => import("./Pages/NotFound404"));
+const HeroSectionPage = lazy(() => import("./Pages/HeroSectionPage"));
+const HomePage = lazy(() => import("./Pages/HomePage"));
+
+squircle.register();
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/bg-page" element={<BgPage />} />
-          <Route path="/text-page" element={<TextPage />} />
-          <Route path="/spinner-page" element={<SpinnerPage />} />
-          <Route path="/dashboard-items" element={<DashBoardItems />} />
-          <Route path="/accordion-items" element={<DropDownPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/tables-page" element={<TablesPage />} />
-          <Route path="/button-page" element={<ButtonsPage />} />
-          <Route path="/calendar-page" element={<CalendarPage />} />
-          <Route path="/toolbar-page" element={<ToolBarPage />} />
-          <Route path="/alert-page" element={<AlertPage />} />
-          <Route path="/tailwind-resources-page" element={<TailwindSourcesPage />} />
-          <Route path="/profile-page" element={<ProfilePage />} />
-
-          {/*<Route path="/html-editor-page" element={<HtmlEditor />} />*/}
-          <Route path="/hero-section" element={<HeroSectionPage />} />
-
-          
-          
-          <Route path="/test" element={<BlankPage />} />
-
-
-          <Route path="*" element={<NotFound404 />} />
-
-        </Route>
-      </Routes>
+      <Suspense fallback={
+        <div className="flex justify-center items-center fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <l-squircle
+          size="37"
+          stroke="5"
+          stroke-length="0.15"
+          bg-opacity="0.1"
+          speed="0.9"
+          color="black"
+        ></l-squircle>
+      </div>
+      }>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/bg-page" element={<BgPage />} />
+            <Route path="/text-page" element={<TextPage />} />
+            <Route path="/spinner-page" element={<SpinnerPage />} />
+            <Route path="/dashboard-items" element={<DashBoardItems />} />
+            <Route path="/accordion-items" element={<DropDownPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/tables-page" element={<TablesPage />} />
+            <Route path="/button-page" element={<ButtonsPage />} />
+            <Route path="/calendar-page" element={<CalendarPage />} />
+            <Route path="/toolbar-page" element={<ToolBarPage />} />
+            <Route path="/alert-page" element={<AlertPage />} />
+            <Route path="/tailwind-resources-page" element={<TailwindSourcesPage />} />
+            <Route path="/profile-page" element={<ProfilePage />} />
+            <Route path="/hero-section" element={<HeroSectionPage />} />
+            <Route path="/test" element={<BlankPage />} />
+            <Route path="*" element={<NotFound404 />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,37 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-plugins: [
+  build: {
+    chunkSizeWarningLimit: 5000, // Set to a larger value (in KB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Chunking for node_modules to create a vendor chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          // Add specific pages or components to separate chunks
+          if (id.includes('Pages/')) {
+            const pageName = id.split('/').pop().replace('.jsx', ''); // Get the page name from the path
+            return `page-${pageName}`;
+          }
+          // You can also add more chunking conditions for other parts of the application, like libraries or utils
+          if (id.includes('utils')) {
+            return 'utils';
+          }
+          if (id.includes('components')) {
+            return 'components';
+          }
+        },
+      },
+    },
+  },
+  plugins: [
     react(),
     VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+      },
       registerType: "autoUpdate",
       includeAssets: ["OneUI.png", "robots.txt"],
       manifest: {
@@ -26,7 +54,7 @@ plugins: [
   optimizeDeps: {
     include: [
       '@ag-grid-community/core',
-      
-    ] 
- }
+      // Add other dependencies that should be optimized here
+    ]
+  }
 })
